7ea219761795b5fc50a937194d091f4ded02b913
John Manack <jmnck28@gmail.com>
2021-01-08 16:14:10 -0500

    updates js to accept form entries

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,22 @@
 'use strict';
 
-console.log('Scripts Loaded');
+console.log('Sweet sweet JS Loaded');
 
 const generateMadlib = document.querySelector('#generateMadlib');
 
 generateMadlib.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    const inputSelectors = document.querySelectorAll('input')
+    const inputSelectors = document.querySelectorAll('input');
+    const nameResponse = document.querySelector('#nameResponse');
+    const subjectResponse = document.querySelector('#subjectResponse');
 
     inputSelectors.forEach(function (inputItem) {
-        console.log("What is the input item:", inputItem)
-    })
-
+        if (inputItem.name === 'myName') {
+            nameResponse.innerHTML = inputItem.value;
+        }
+        if (inputItem.name === 'mySubject') {
+            subjectResponse.innerHTML = inputItem.value;
+        }
+    });
 })
